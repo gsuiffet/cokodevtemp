6f3fe0c178657200e658a61799b72a38e31648b1
Guillaume Suiffet <suiffet.g@gmail.comm>
2017-10-21 04:22:57 +0200

    Implementation de la maquette

---
+++ public/js/myscript.js
@@ -0,0 +1,13 @@
+console.log("Hey");
+$( document ).ready(function() {
+    $( "#p1Next" ).click(function() {
+        $('.easy1').slideToggle();
+        $('.easy2').slideToggle();
+    });
+    $( "#p2Back" ).click(function() {
+        $('.easy1').slideToggle();
+        $('.easy2').slideToggle();
+    });
+});
+
+
